5ddeb80e20ecaa4cef22736710bff399083a5e04
AnnaBLoz <anninha_beatriz@terra.com.br>
2025-11-23 17:12:39 -0300

    ajuste csproj testes

--- obj/Debug/net8.0/freela-match-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/freela-match-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("freela-match-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97687af0536b99532beaa266fe7c8703ad0a225f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+586a375abc697e6000b12ce1e0f6e81f40a5ef4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("freela-match-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("freela-match-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
