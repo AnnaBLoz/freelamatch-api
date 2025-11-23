b0a3d798e342fcebbcd1ad042fa1dd62eaeebe42
AnnaBLoz <anninha_beatriz@terra.com.br>
2025-11-23 02:30:21 -0300

    Ajuste SONAR

--- obj/Debug/net8.0/freela-match-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/freela-match-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("freela-match-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d057012ee12e14e3f7195e648d0b92a97c6a8042")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a6efca2fefc5e346dc4092235afd8b910cc824b")]
 [assembly: System.Reflection.AssemblyProductAttribute("freela-match-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("freela-match-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
