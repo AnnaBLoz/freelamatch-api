5053bc2b2001825637906eb77c1b4d8065138d55
AnnaBLoz <anninha_beatriz@terra.com.br>
2025-11-24 01:30:22 -0300

    ajuste sonar

---
+++ freela-match-api-test/sanityTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace freela_match_api_test
+{
+    public class SanityTest
+    {
+        [Fact]
+        public void Should_Run()
+        {
+            Assert.True(true);
+        }
+    }
+}
