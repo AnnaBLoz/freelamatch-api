c7e9c1400ca9e3be6373c03760a314d9944aa3ac
AnnaBLoz <anninha_beatriz@terra.com.br>
2025-11-29 01:47:31 -0300

    Coverage - DeepSource

--- freela-match-api-test/obj/Debug/net8.0/freela-match-api-test.AssemblyInfo.cs
+++ freela-match-api-test/obj/Debug/net8.0/freela-match-api-test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("freela-match-api-test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+386f7f112c2e8c8fe680cf27043099008a2fc661")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3f4c54a08d58d9a448bc91142974ec77441a6dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("freela-match-api-test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("freela-match-api-test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
